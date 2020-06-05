724695a8169617f2b76eca484410a430c365f9ef
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-05 20:21:05 +0530

    Added state, basic setup new component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 
 
 class App extends Component {
+  state = {
+    todos:[
+      {id:1, content:'Buy milk'},
+      {id:2, content:'Play Mario Kart'}
+    ]
+  }
   render()
     {
       return (
